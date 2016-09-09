efcac1d355ab2764e0f281cd36d07506d652ccf4
Baik <Baik@DESKTOP-0LTD7CH.telus>
2016-09-08 23:21:33 -0700

    Added comments

--- Student.php
+++ Student.php
@@ -7,12 +7,13 @@
  */
 
 /**
- * Description of Student
+ * Creating the Student class defining each variables
  *
  * @author Baik
  */
 class Student {
     
+    //Student Constructor contains name, emails, and grades
     function __construct() {
         $this->surname      = '';
         $this->first_name   = '';
@@ -20,14 +21,17 @@ class Student {
         $this->grades       = array();
     }
     
+    //function to add emails to the array
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
-
+    
+    //function to add grades to the array
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
-
+    
+    //function that makes average of added grades from the array
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -35,6 +39,7 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //print out all gathered information of the person by format of name average and email
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
