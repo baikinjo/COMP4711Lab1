96b1e26fee9850982273618c79d1b26a30b102b4
Baik <Baik@DESKTOP-0LTD7CH.telus>
2016-09-08 23:21:25 -0700

    Added custom entries, with comments

--- index.php
+++ index.php
@@ -11,10 +11,11 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        include('Student.php');
+        include('Student.php'); //recalls method from Student.php class
 
-        $students = array();
+        $students = array(); //declare an array
         
+        //first entry of the array provided by instructor
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -25,6 +26,7 @@ and open the template in the editor.
         $first->add_grade(55);
         $students['j123'] = $first;
         
+        //second entry of the array provided by instructor
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -36,8 +38,29 @@ and open the template in the editor.
         $second->add_grade(50);
         $students['a456'] = $second;
         
-        ksort($students);
+        //third entry of the array custom-created
+        $third = new Student();
+        $third->surname = "Stark";
+        $third->first_name = "Tony";
+        $third->add_email('work', 'tstark@starkindustry.com');
+        $third->add_grade(98);
+        $third->add_grade(96);
+        $third->add_grade(100);
+        $students['t123'] = $third;
         
+        //fourth entry of the array custom-created
+        $fourth = new Student();
+        $fourth->surname = "Baik";
+        $fourth->first_name = "Injo";
+        $fourth->add_email('work', 'ibaik@my.bcit.ca');
+        $fourth->add_grade(60);
+        $fourth->add_grade(70);
+        $fourth->add_grade(80);
+        $students['i123'] = $fourth;
+        
+        ksort($students); //sort by name
+        
+        //to call out all the list from the array
         foreach ($students as $student) {
             echo $student->toString();
         }
